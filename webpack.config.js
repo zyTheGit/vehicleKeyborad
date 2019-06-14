e6ff0cdd9cb7f598d99b66af862e7cec826414c1
zyTheGit <34878821+zyTheGit@users.noreply.github.com>
2019-06-14 13:21:26 +0800

    Update webpack.config.js
    解决之前版本不能绑定在window的问题，这一版本不需要这个选项，可以使用import 导出

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,8 @@ module.exports = {
   entry: './html/keyborad.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'keyboard.js'
+    filename: 'keyboard.js',
+	 // globalObject: "this", //解决之前版本不能绑定在window的问题，这一版本不需要这个选项
   },
   module: {
     rules: [{
